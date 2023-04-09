9adaf2bc162d2fb3698c26244a544368a5373029
404salad <110289934+404salad@users.noreply.github.com>
2023-04-10 00:19:48 +0530

    initial
    will add more stuff

---
+++ magic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# directory management
+cd ~
+sudo mkdir 	
+# essentials apps
+sudo apt install neovim
+sudo apt install firefox
+sudo apt install vlc
+sudo apt install audacity
+sudo apt install gimp
+sudo apt install gedit
+
+# coding
+sudo apt install tmux
+sudo apt install build-essential 
+sudo apt install rustc
+sudo apt install cargo
+sudo cargo install porsmo
+
+#for gnome themes	
+sudo apt install gnome-tweaks
+
+# tail
+sudo apt update
+sudo restart now
